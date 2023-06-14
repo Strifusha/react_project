750858ff5b1eeb2d32bd47c463206646e1943d6a
Strifusha <KatushaStrifusha@gmail.com>
2023-06-14 22:00:14 +0300

    added title refactoring

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,10 +2,12 @@ import handlerMoreDetails from './components/handlerMoreDetails/handlerMoreDetai
 import createCounter from 'pokedex/src/components/createCounter/createCounter';
 import getPokemons from './components/getPokemons/getPokemons';
 import findPokemon from './components/findPokemon/findPokemon';
+import title from'./components/title/title.css';
 
 function App() {
   return (
     <div>
+      <h1 class="pokedex-title">Pokedex</h1>
       <handlerMoreDetails/>
       <createCounter/>
       <getPokemons/> 
